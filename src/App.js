d9b9a061aa53d3ce29aecb1c699542ac395c5594
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-12 15:54:57 +0200

    basic settings

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default App;
